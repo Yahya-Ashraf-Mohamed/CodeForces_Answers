68ef30b7f97660212a36c3b3dc62b0acf387edd9
Yahya-Ashraf-Mohamed <s-yahya.mohamed@zewailcity.edu.eg>
2023-01-10 20:35:44 +0200

    Add Day 6, 7, 8, 9, 10 solutions

---
+++ A Problems/A. Is your horseshoe on the other hoof.cpp
@@ -0,0 +1,21 @@
+﻿// {CF228-D2-A} http://codeforces.com/contest/228/problem/A
+
+#include <iostream>
+using namespace std;
+#include <algorithm>
+#define max 4
+int main() {
+	
+	int NumToBuy = 0;
+	long long Colors_Of_Horseshoes[max];
+	for (int i = 0; i < max; i++)
+		cin >> Colors_Of_Horseshoes[i];
+
+	sort(Colors_Of_Horseshoes, Colors_Of_Horseshoes+max);
+	for (int i = 0; i < max; i++)
+		if (Colors_Of_Horseshoes[i] == Colors_Of_Horseshoes[i + 1])
+			NumToBuy++;
+	cout << NumToBuy;
+ 
+}
+
---
+++ A Problems/A. Juicer.cpp
@@ -0,0 +1,35 @@
+﻿// {CF709-D2-A} http://codeforces.com/contest/709/problem/A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int NumOfOranges, MaxOrangeSize, TrasSize;
+	
+	cin >> NumOfOranges >> MaxOrangeSize >> TrasSize;
+
+	int Orange;
+	int EmpyTrachCounter = 0;
+	int SizeOfTrashs = 0;
+
+	for (int i = 0; i < NumOfOranges; i++)
+	{
+		cin >> Orange;
+		
+		if (Orange <= MaxOrangeSize)
+			SizeOfTrashs += Orange;
+			
+		if (SizeOfTrashs > TrasSize)
+		{
+			EmpyTrachCounter++;
+			SizeOfTrashs = 0;
+		}
+
+		
+	}
+	cout << EmpyTrachCounter << endl;
+
+
+	return 0;
+}

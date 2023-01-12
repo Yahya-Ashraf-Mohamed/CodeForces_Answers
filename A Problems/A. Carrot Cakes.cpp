f8796b43603eaa03638c16f56a7ad770049fdfd0
Yahya-Ashraf-Mohamed <s-yahya.mohamed@zewailcity.edu.eg>
2023-01-12 09:34:23 +0200

    Add Day 11 & 12 solutions

---
+++ A Problems/A. Carrot Cakes.cpp
@@ -0,0 +1,19 @@
+﻿// {CF799-D2-A} http://codeforces.com/contest/799/problem/A
+
+#include <iostream>
+using namespace std;
+#include <math.h>
+
+int main() {
+	
+	double  NumOfCakes, BakeTime, NumOfBackedAtTime, Build2OvenTime;
+	cin >> NumOfCakes >> BakeTime >> NumOfBackedAtTime >> Build2OvenTime;
+	int T = BakeTime * ceil(NumOfCakes / NumOfBackedAtTime);
+	if ( T > (Build2OvenTime + BakeTime))
+		cout << "YES";
+	else
+		cout << "NO";
+
+
+	return 0;
+}

821b2c7453c5730803e3a7c41a5b25533fcbbb31
Yahya-Ashraf-Mohamed <s-yahya.mohamed@zewailcity.edu.eg>
2023-01-13 19:37:21 +0200

    Add Day 13 solutions

---
+++ A Problems/A. Free Ice Cream.cpp
@@ -0,0 +1,33 @@
+﻿// {CF71-D2-A} http://codeforces.com/contest/71/problem/A
+
+#include <iostream>
+using namespace std;
+#include <string>
+
+#define ll long long
+int main() {
+	
+	ll NumberOfOperations, NumPacksIcecream;
+
+	cin >> NumberOfOperations >> NumPacksIcecream;
+
+	char Operation;
+	ll Packs;
+	ll NumSadKids = 0;
+
+	for (ll i = 0; i < NumberOfOperations; i++)
+	{
+		cin >> Operation >> Packs;
+		
+		if (Operation == '-')
+			if (NumPacksIcecream >= Packs)
+				NumPacksIcecream -= Packs;
+			else
+				NumSadKids++;
+		else
+			NumPacksIcecream += Packs;
+	}
+
+	cout << NumPacksIcecream << " " << NumSadKids;
+	return 0;
+}
